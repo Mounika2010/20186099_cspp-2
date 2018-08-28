55fb3118efe4bda98d42af861d04dbe116a756e8
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-28 23:31:48 +0530

    quotient & remainder

---
+++ cspp2_practice/m1/a13/Quotient.java
@@ -0,0 +1,21 @@
+/**
+ * @author : Mounika2010.
+ */
+public class Quotient {
+	/**
+	 * quotirnt of two numbers.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 2;
+		int quot = a / b;
+		int rem = a % b;
+		System.out.println(quot);
+		System.out.println(rem);
+
+
+	}
+
+}
