527ed42a084c82e318764d00b983eecedd2ca135
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-28 19:39:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2_assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ cspp2_assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -3,42 +3,40 @@ import java.util.Scanner;
  * Roots of a quadratic equation.
  */
 public final class Solution {
-	/**
-	 * Quadratic equation.
-	 *
-	 * @param      args  The arguments
-	 */
-	private Solution() {
-		//constructor.
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		roots(a, b, c);
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	public static void roots(final int a, final int b, final int c) {
-		int det = (b * b) - (2 * 2 * a * c);
-		double root1 = (-b + Math.sqrt(det)) / (2 * a);
-		double root2 = (-b - Math.sqrt(det)) / (2 * a);
-		System.out.println(root1 + " " + root2);
-		}
-	/*
-	Need to write the roots function and print the output.
-	*/
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    public static void roots(final int a, final int b, final int c) {
+        int det = (b * b) - (2 * 2 * a * c);
+        double root1 = (-b + Math.sqrt(det)) / (2 * a);
+        double root2 = (-b - Math.sqrt(det)) / (2 * a);
+        System.out.println(root1 + " " + root2);
+        }
+    /*
+    Need to write the roots function and print the output.
+    */
 }
 
 
