98e548a0910d310f868371c4eaa80760f5d0eae8
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-28 23:33:52 +0530

    degrees to fahrenheit

---
+++ cspp2_practice/m1/a14/degrees_to_fahrenheit.java
@@ -0,0 +1,17 @@
+/**
+ * @author : Mounika2010.
+ */
+public final class degrees_to_fahrenheit {
+	/**
+	 * converting deg to fahrenheit.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		int a = 40;
+		double f = ((a * 1.8) + 32);
+		System.out.println(f);
+		
+	}
+
+}
