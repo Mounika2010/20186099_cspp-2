f531fe46c2d6258c182b15519bafe8c212081932
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-28 18:11:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ cspp2_assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -14,8 +14,8 @@ public class Solution {
 	}
 	public static void rootsOfQuadraticEquation(int a, int b, int c) {
 		int det = (b*b) - (4*a*c);
-		double root1 = (-b + Math.sqrt(det))/2*a;
-		double root2 = (-b - Math.sqrt(det))/2*a;
+		double root1 = (-b + Math.sqrt(det))/(2*a);
+		double root2 = (-b - Math.sqrt(det))/(2*a);
 		System.out.println(root1 + " " + root2);
 		}
 		
