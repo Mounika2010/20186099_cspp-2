3a82f4bc2b4d3e11205b075c936528f3b0d3e7f4
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-30 15:42:58 +0530

    average

---
+++ cspp2_practice/m4/a11/Average.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/**
+* @author     SupriyaMadupathi
+*/
+public final class Average {
+   /**.
+    * Constructs the object.
+    */
+   private Average() {
+   }
+   /**
+   * Main function to print.
+   * @param args is the parameter for this function
+   */
+    public static void main(final String[] args) {
+       int n, sum = 0;
+       float avg;
+       Scanner s = new Scanner(System.in);
+       n = s.nextInt();
+       int[] arr = new int[n];
+       for (int i = 0; i < n; i++) {
+           arr[i] = s.nextInt();
+           sum = sum + arr[i];
+       }
+       avg = (float) sum / n;
+       System.out.println("Avarage is :" + avg);
+   }
+}
