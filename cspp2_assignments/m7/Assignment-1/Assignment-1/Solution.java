74fbda36a97413be37bccf91d7d372ed8f27b2e5
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-09-03 20:36:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -1,25 +1,54 @@
+/**
+ * @author : Mounika2010.
+ */
 import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
 class InputValidator {
+	/**
+	 * variable description.
+	 */
 	private String input;
 
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      temp  The temporary
+	 */
 	InputValidator(final String temp) {
 		this.input = temp;
 	}
+	/**
+	 * returns true or false.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	boolean validateData() {
-	final int z = 6;
-	if (input.length() >= z) {
+	final int a = 6;
+	if (input.length() >= a) {
 		return true;
 		}
 		return false;
 	}
 
 }
+/**
+ * class for solution.
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 		//constructor is used.
 	}
-	public static void main(final String args[])
-    {
+	/**
+	 * Main function.
+	 * 
+	 * @param      args  The temporary     
+	 */
+	public static void main(final String[] args) {
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
     	InputValidator i = new InputValidator(input);    	
@@ -28,3 +57,6 @@ public final class Solution {
     }
 
 }
+
+
+
