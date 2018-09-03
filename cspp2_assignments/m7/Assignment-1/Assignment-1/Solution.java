09acf54de65a1fe9e008832bba6b37e95c3a38b4
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-09-03 20:37:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -6,53 +6,53 @@ import java.util.Scanner;
  * Class for input validator.
  */
 class InputValidator {
-	/**
-	 * variable description.
-	 */
-	private String input;
+    /**
+     * variable description.
+     */
+    private String input;
 
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      temp  The temporary
-	 */
-	InputValidator(final String temp) {
-		this.input = temp;
-	}
-	/**
-	 * returns true or false.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	boolean validateData() {
-	final int a = 6;
-	if (input.length() >= a) {
-		return true;
-		}
-		return false;
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      temp  The temporary
+     */
+    InputValidator(final String temp) {
+        this.input = temp;
+    }
+    /**
+     * returns true or false.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    boolean validateData() {
+    final int a = 6;
+    if (input.length() >= a) {
+        return true;
+        }
+        return false;
+    }
 
 }
 /**
  * class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//constructor is used.
-	}
-	/**
-	 * Main function.
-	 * 
-	 * @param      args  The temporary     
-	 */
-	public static void main(final String[] args) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor is used.
+    }
+    /**
+     * Main function.
+     * 
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
