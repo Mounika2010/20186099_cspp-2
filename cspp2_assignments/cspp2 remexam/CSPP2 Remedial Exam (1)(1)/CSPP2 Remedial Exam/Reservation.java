40409b8a95066515b70a781bf53ba8bc556f3a69
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-12-25 12:18:30 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ cspp2_assignments/cspp2 remexam/CSPP2 Remedial Exam (1)(1)/CSPP2 Remedial Exam/Reservation.java
@@ -0,0 +1,101 @@
+/*
+  Reservation class, stores the person and room number in the Hotel
+*/
+
+public class Reservation{
+    //instance variables
+    private String name;
+    private int roomNumber;
+
+    //constructors, must supply the name, and optionally a room
+    public Reservation(String person){
+    	this.name = person;
+
+    }
+    public Reservation(String person, int room){
+        this.name = person;
+        this.roomNumber = room;
+
+
+    }
+
+    //mutators, set the room number or name
+    public void setRoom(int newroom){
+    	this.roomNumber = newroom;
+    }
+
+    public void setName(String newname){
+    	this.name = newname;
+    }
+
+    //accessors, return the room number or name
+    public int getRoom(){
+        return this.roomNumber;
+    	
+    }
+
+    public String getName(){
+        return this.name;
+    	
+    }
+
+    public String toString() {
+        return name + " " + roomNumber;
+    }
+
+    
+}
+
+class Hotel {
+
+    private String[] hotelrooms;
+    private int size;
+ 
+
+    public Hotel () {
+        hotelrooms = new String[5];
+        size = 0;
+
+
+    }
+
+    public int reserveRoom(String person) {
+    
+        for (int i = 0; i < hotelrooms.length; i++) {
+            if (hotelrooms[i] != null) {
+                int k = 0+1;
+
+            } else {
+                hotelrooms[i] = person;
+                size++;
+                int p = i+1;
+                return p;
+            }
+            
+        }
+        return -1;
+    }
+
+    public boolean reserveRoom(String person, int roomNum) {
+        return true;
+
+    }
+
+    public void printReservations() {
+
+    }
+
+    public void cancelReservations(String person) {
+
+    }
+
+    public void buildRooms(int num) {
+
+    }
+
+
+}
+
+   
+
+   
