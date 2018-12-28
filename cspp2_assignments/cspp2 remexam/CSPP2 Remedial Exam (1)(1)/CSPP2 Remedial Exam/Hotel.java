d5e2d343d454dfc80b7ab1094031b1cce43805b6
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-12-28 11:22:01 +0530

    "cspp2_m10_a1_1: 1/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- cspp2_assignments/cspp2 remexam/CSPP2 Remedial Exam (1)(1)/CSPP2 Remedial Exam/Hotel.java
+++ cspp2_assignments/cspp2 remexam/CSPP2 Remedial Exam (1)(1)/CSPP2 Remedial Exam/Hotel.java
@@ -1,75 +1,58 @@
 import java.util.*;
-public class Hotel{
-
-	Reservation[]rooms;
+class Hotel{
+	Reservation[] hotelrooms;
 	int size;
 
-	public Hotel(){
-		rooms = new Reservation[6];
-		size = 1;
+	public Hotel() {
+		size = 0;
+		hotelrooms = new Reservation[6];
 	}
-	public int size(){
-		return size;
-	}
-	public int reserveRoom(String person){
-		if (size == rooms.length) {
-			System.out.println("All Rooms are reserved");
+
+	public int reserveRoom(String person) {
+		if (size == hotelrooms.length) {
+			System.out.println("All rooms are reserved");
 		}
-		int i = 1;
-		for ( i = 1;i <rooms.length;i++) {
-			if (rooms[i]== null) {
-				rooms[i] = new Reservation(person,i);
+		for (int i = 1; i < hotelrooms.length; i++) {
+			if(hotelrooms[i] == null) {
+				hotelrooms[i] = new Reservation(person, i);
 				size++;
 				return i;
-			}
+ 			}
+			
 		}
 		return -1;
-		
+
 	}
-	public boolean isEmpty(int roomNumber){
-		for (int i = 1;i < rooms.length;i++){
-			if (rooms[roomNumber]== null) {
-				return true;
-			}
-		}
-		return false;
-	}
-	public boolean reserveRoom(String person,int roomNumber){
-		if (size == rooms.length) {
-			System.out.println("All Rooms are reserved");
+
+	public boolean reserveRoom(String person, int roomNum) {
+		if (size == hotelrooms.length) {
+			System.out.println("All rooms are reserved");
 			return false;
-		}else if(!isEmpty(roomNumber)){
+
+		} else if (hotelrooms[roomNum] != null) {
 			System.out.println("Room is already reserved");
 			return false;
 		} else {
-			rooms[roomNumber] = new Reservation(person,roomNumber);
+			hotelrooms[roomNum] = new Reservation(person, roomNum);
 			size++;
+			return true;
 		}
+
+	}
+
+	public void printReservations() {
+		for (int i = 1; i < hotelrooms.length; i++) {
+			if (hotelrooms[i] != null) {
+				System.out.println(hotelrooms[i]);
+			}
+		}
+	}
+
+	public void cancelReservations(String person) {
+
+	}
+
+	public boolean buildRooms(int num) {
 		return true;
 	}
-	public void printReservations(){
-		for (int i = 1;i < rooms.length;i++) {
-			if (rooms[i]!= null) {
-				System.out.println(rooms[i]);
-			}
-		}
-	}
-	public void cancelReservations(String person){
-		for (int i = 0;i < size ;i++) {
-			if (rooms[i]!= null) {
-				if (rooms[i].getpersonName().equals(person)) {
-					rooms[i] = null;
-				}
-			}
-			
-		}
-	}
-		public boolean buildRooms(int num) {
-			Reservation[] temp = new Reservation[rooms.length+num];
-			System.arraycopy(rooms,0,temp,0,rooms.length);
-			rooms = temp;
-			return true;
-			}
-
-
 }
