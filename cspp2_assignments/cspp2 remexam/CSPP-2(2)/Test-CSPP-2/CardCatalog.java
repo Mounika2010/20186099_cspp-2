d881279472ace2da947ab867c89f5a5178afd310
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-12-31 18:02:37 +0530

    cardcatalog

---
+++ cspp2_assignments/cspp2 remexam/CSPP-2(2)/Test-CSPP-2/CardCatalog.java
@@ -0,0 +1,31 @@
+public class CardCatalog {
+
+	int maxcards;
+	int size;
+	Card[20] cards;
+
+	CardCatalog(int maxcards) {
+		this.maxcards = 0;
+		size = 0;
+
+	}
+
+	public int addACard(Card c1) {
+		for (i = 0; i < cards.length; i++) {
+			if(cards = null) {
+				cards[i] = c1;
+				++size;
+			}			
+		}
+
+	}
+
+	public int sizeOfCatalog() {
+		return size;
+	}
+
+	public void removeCard(String bookTitle) {
+		
+	}
+
+}
